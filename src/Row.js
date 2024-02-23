7e753e1027ee8a900b9aa1404786a6fba65e2381
siddharth <siddharth@scaler.com>
2024-02-24 01:10:49 +0530

    name

--- src/Row.js
+++ src/Row.js
@@ -34,7 +34,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     if (trailerUrl) {
       setTrailerUrl('')
     } else {
-      movieTrailer(movie?.title || "")
+      movieTrailer(movie?.title || movie?.name || "")
         .then(url => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get('v'));
